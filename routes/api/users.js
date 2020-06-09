d99617be1acd3414f229ef5a9aa833c4e715b4e0
SenuraJayadeva <53615169+SenuraJayadeva@users.noreply.github.com>
2020-06-09 19:10:01 +0530

    completed gravatar and send user registration details to the mongodb atlas

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
+
+//Bring User Model
+const User = require("../../models/User");
 
 //@route  POST api/users
 //@desc   register user
@@ -15,15 +20,58 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    console.log(req.body);
-    res.send("User route");
+    const { name, email, password } = req.body;
+
+    try {
+      //See if user Exist
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exist" }] });
+      }
+
+      //get users gravatar(if the user is not found)
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      //create a user instance
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //Encrypt Password
+
+      //10 is enogh..if you want more secured.user a value more than 10
+      const salt = await bcrypt.genSalt(10);
+
+      //hashing password
+      user.password = await bcrypt.hash(password, salt);
+
+      //save user to the database
+      await user.save();
+
+      //Return jsonwebtoken
+      res.send("User Registered");
+    } catch (err) {
+      //Something wrong with the server
+      console.error(err.message);
+      return res.status(500).send("Server Error");
+    }
   }
 );
 
