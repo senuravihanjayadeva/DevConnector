d5ca1e84cdaa15ed62f96fddda6831b8cab4023a
SenuraJayadeva <53615169+SenuraJayadeva@users.noreply.github.com>
2020-06-09 13:22:08 +0530

    set validations for user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,9 +5,26 @@ const { check, validationResult } = require("express-validator");
 //@route  POST api/users
 //@desc   register user
 //@access Public
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("User route");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    console.log(req.body);
+    res.send("User route");
+  }
+);
 
 module.exports = router;
