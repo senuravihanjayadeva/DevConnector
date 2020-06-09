9e573d4b775e52e24df03630cf495326b58b1193
SenuraJayadeva <53615169+SenuraJayadeva@users.noreply.github.com>
2020-06-09 11:41:01 +0530

    setting routes

--- config/db.js
+++ config/db.js
@@ -4,6 +4,9 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
+    /*
+    The await expression causes async function execution to pause until a Promise is settled (that is, fulfilled or rejected), and to resume execution of the async function after fulfillment. When resumed, the value of the await expression is that of the fulfilled Promise.If the Promise is rejected, the await expression throws the rejected value.
+    */
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
---
+++ routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+//@route  POST api/users
+//@desc   register user
+//@access Public
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("User route");
+});
+
+module.exports = router;
