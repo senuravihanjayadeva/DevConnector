5c97b13b28c25123c9a61ac1d48e351603b54bd4
SenuraJayadeva <53615169+SenuraJayadeva@users.noreply.github.com>
2020-06-10 13:25:48 +0530

    User Authentication / Login Route

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, next) {
   //Get the token from the header
   const token = req.header("x-auth-token");
 
-  //Check if not token
+  //Check if no token
   if (!token) {
     return res.status(401).json({ msg: "No token, authorization denied" });
   }
@@ -22,7 +22,7 @@ module.exports = function (req, res, next) {
 
     next();
   } catch (err) {
-    //There is a token but its not valid
+    //There is a token, but its not valid
     res.status(400).json({ msg: "Token is not valid" });
   }
 };
