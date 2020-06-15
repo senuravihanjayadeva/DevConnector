b57a01068448133b06843ba540d183e32511f7c8
SenuraJayadeva <53615169+SenuraJayadeva@users.noreply.github.com>
2020-06-15 17:41:02 +0530

    add PROFILE Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,7 @@ const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const { json } = require("express");
 
 //@route  GET api/profile/me
 //@desc   Get current users profile
@@ -162,5 +163,62 @@ router.delete("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+//@route  PUT  api/profile/experience
+//@desc  Add profile experience
+//@access private
+
+router.put(
+  "/experience",
+  [
+    auth,
+
+    [
+      check("title", "Title is required").not().isEmpty(),
+      check("company", "Company is required").not().isEmpty(),
+      check("from", "From Data is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
